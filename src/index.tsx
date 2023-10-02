ea730fa209fb071ed3a41cc075290636f8602009
Esther Falayi <esther.falayi@gmail.com>
2023-10-02 17:52:46 +0100

    (initial): setup
    - setup project with CRA
    - install prettier and extend eslint config

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/* TO DO
+1. ESlint config
+2. Husky setup
+3. Tailwind setup (and/or styled components)
+4. Dark mode setup
+5. Fonts
+ */
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
