70fbf65acb09ca201c6e2ae9164873dd2088f203
Esther Falayi <esther.falayi@gmail.com>
2023-10-11 13:53:56 +0100

    (components): movies
    - setup tailwindcss
    - create MovieCard component
    - add eslintignore file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
-import './App.css';
-
 import MovieCard, { IMovieCardProps } from './components/MovieCard';
 import { getMovies } from './api/movies.api';
 
@@ -33,29 +31,43 @@ function App() {
     };
   }, [loading]);
 
+  console.log('movies: ', movies);
+
   return (
-    <div className="app">
-      <header className="app-header">
-        <h1>Flicks Deck</h1>
+    <div className="bg-white">
+      <header className="my-8">
+        <h1 className="text-3xl text-center font-bold underline">
+          Flicks Deck
+        </h1>
       </header>
-      <section>
-        <form onSubmit={handleMovieSearch}>
+      <section className="mx-auto max-w-2xl px-4 py-8 sm:px-6 lg:px-8">
+        <form className="w-full flex gap-x-4" onSubmit={handleMovieSearch}>
           <input
+            className="block w-full rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             type="search"
             id="search"
-            placeholder="Search"
+            placeholder="Enter movie / series title..."
             name="searchTitle"
             value={searchTitle}
             onChange={handleInputChange}
           />
-          <button>Search</button>
+          <button className="pointer-events-auto rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold leading-5 text-white hover:bg-indigo-500">
+            Search
+          </button>
         </form>
       </section>
-      <section>
-        {movies.map((movie: IMovieCardProps) => {
-          return <MovieCard key={movie.imdbID} {...movie} />;
-        })}
-      </section>
+
+      <div className="mx-auto max-w-2xl px-4 py-8 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          Movies
+        </h2>
+
+        <section className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+          {movies.map((movie: IMovieCardProps) => {
+            return <MovieCard key={movie.imdbID} {...movie} />;
+          })}
+        </section>
+      </div>
     </div>
   );
 }
--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -8,8 +8,22 @@ export interface IMovieCardProps {
 
 function MovieCard(props: IMovieCardProps) {
   return (
-    <div>
-      <span>{props.Title}</span>
+    <div className="group relative">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+        <img
+          src={props.Poster}
+          alt={props.Title}
+          className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+        />
+      </div>
+      <div className="mt-4 flex">
+        <span className="block">
+          <h3 className="text-sm text-gray-700">
+            <span>{props.Title}</span>
+          </h3>
+          <p className="mt-1 text-sm text-gray-500">{props.Year}</p>
+        </span>
+      </div>
     </div>
   );
 }
